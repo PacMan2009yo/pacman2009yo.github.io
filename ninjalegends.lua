89a174f515c4a97303ef12b057be9129223c09e6
PacMan2009yo <41653862+PacMan2009yo@users.noreply.github.com>
2021-01-24 10:02:19 -0500

    Update ninjalegends.lua

--- ninjalegends.lua
+++ ninjalegends.lua
@@ -1,8 +1,8 @@
 -- bruh
 
 local ui_options = {
-	main_color = Color3.fromRGB(41, 74, 122),
-	min_size = Vector2.new(400, 300),
+	main_color = Color3.fromRGB(255, 0, 0),
+	min_size = Vector2.new(470, 300),
 	toggle_key = Enum.KeyCode.RightShift,
 	can_resize = true,
 }
