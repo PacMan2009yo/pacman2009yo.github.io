ce417e1f0569307a5b42760c2f3fa447898d87e8
PacMan2009yo <41653862+PacMan2009yo@users.noreply.github.com>
2021-02-01 17:12:48 -0500

    Update main.php

--- wl/main.php
+++ wl/main.php
@@ -14,7 +14,7 @@
     $HWID = getallheaders()["Syn-Fingerprint"];
     
     // This method is really bad, use arrays.
-    if ($Key == '1234567890' and $HWID == 'd0a5102a78f69a74e26fabbca4c9dee21425aca560ac71f459f43b8b5bc0065d2636c53a23e4da55b8b9b43893cf7e781d22d5e295f30909df9757eac211cd99') {
+    if ($Key == '1234567890' and $HWID == 'ed0a5102a78f69a74e26fabbca4c9dee21425aca560ac71f459f43b8b5bc0065d2636c53a23e4da55b8b9b43893cf7e781d22d5e295f30909df9757eac211cd99') {
         echo base64_encode($Key);
     } else {
         echo rString(rand(1, 999));
